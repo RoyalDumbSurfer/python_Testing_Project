ff4a79067e2b6237c5ac30b90a03e71e263ff956
RoyalDumbSurfer <comradodean@gmail.com>
2021-10-15 13:03:54 +0800

    Дано несколько чисел. Вычислите их сумму. Сначала вводите количество чисел N, затем вводится ровно N целых чисел.

---
+++ sum_of_N.py
@@ -0,0 +1,13 @@
+# count = int(input('how many numbers? '))
+# x = 0
+# for i in range(count):
+#     number = int(input())
+#     x += number
+#
+# print(x)
+
+n = int(input())
+sum = 0
+for i in range(n):
+    sum += int(input())
+print(sum)
