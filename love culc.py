0b89e52104426dd3a9094ccfd9786d5b6c24adc1
RoyalDumbSurfer <comradodean@gmail.com>
2023-08-02 01:04:54 +0800

    love culc optimisation

--- love culc.py
+++ love culc.py
@@ -2,57 +2,26 @@ print("Welcome to the Love Calculator!")
 name1 = input("What is your name? \n")
 name2 = input("What is their name? \n")
 
-name1_lower = name1.lower()
-name2_lower = name2.lower()
+combined_names = name1 + name2
+combined_names_lower = combined_names.lower()
 
-counter_1 = 0
-if name1_lower.count('t'):
-    counter_1 += name1_lower.count('t')
-if name1_lower.count('r'):
-    counter_1 += name1_lower.count('r')
-if name1_lower.count('u'):
-    counter_1 += name1_lower.count('u')
-if name1_lower.count('e'):
-    counter_1 += name1_lower.count('e')
+t = combined_names_lower.count('t')
+r = combined_names_lower.count('r')
+u = combined_names_lower.count('u')
+e = combined_names_lower.count('e')
+true = t + r + u + e
 
-if name2_lower.count('t'):
-    counter_1 += name2_lower.count('t')
-if name2_lower.count('r'):
-    counter_1 += name2_lower.count('r')
-if name2_lower.count('u'):
-    counter_1 += name2_lower.count('u')
-if name2_lower.count('e'):
-    counter_1 += name2_lower.count('e')
+l = combined_names_lower.count('l')
+o = combined_names_lower.count('o')
+v = combined_names_lower.count('v')
+e = combined_names_lower.count('e')
+love = l + o + v + e
 
-counter_2 = 0
-if name1_lower.count('l'):
-    counter_2 += name1_lower.count('l')
-if name1_lower.count('o'):
-    counter_2 += name1_lower.count('o')
-if name1_lower.count('v'):
-    counter_2 += name1_lower.count('v')
-if name1_lower.count('e'):
-    counter_2 += name1_lower.count('e')
+love_index = int(str(true) + str(love))
 
-if name2_lower.count('l'):
-    counter_2 += name2_lower.count('l')
-if name2_lower.count('o'):
-    counter_2 += name2_lower.count('o')
-if name2_lower.count('v'):
-    counter_2 += name2_lower.count('v')
-if name2_lower.count('e'):
-    counter_2 += name2_lower.count('e')
-
-counter_1_str = str(counter_1)
-counter_2_str = str(counter_2)
-
-love_index = int(counter_1_str + counter_2_str)
-
-if love_index < 10 or love_index > 90:
+if (love_index) < 10 or (love_index) > 90:
     print(f"Your score is {love_index}, you go together like coke and mentos.")
-elif 40 < love_index < 50:
+elif 40 <= love_index <= 50:
     print(f"Your score is {love_index}, you are alright together.")
 else:
     print(f"Your score is {love_index}.")
-
-
