8608d05716fa421579482f028ae677b435a0638c
RoyalDumbSurfer <comradodean@gmail.com>
2023-08-04 18:59:14 +0800

    surf paradise lower

--- tresure island.py
+++ tresure island.py
@@ -10,15 +10,14 @@ print("Welcome to Surfers Paradise!")
 name = input("What's your name? ")
 print(f"{name}, your mission is to catch the Barrel on a secret spot!!!")
 
-wave = input("Wave coming! Which wave is it: Left, type 'Left', or Right, type 'Right'? ")
-wave = wave.lower()
+wave = input("Wave coming! Which wave is it: Left, type 'Left', or Right, type 'Right'? ").lower()
 
 if wave == "right":
-    wave_type = input("Barrel, type 'Barrel', or open face, type 'Open face'? ")
-    wave_type = wave_type.lower()
+    wave_type = input("Barrel, type 'Barrel', or open face, type 'Open face'? ").lower()
+
     if wave_type == "barrel":
-        barrel_type = input("Which kind of barrel? choose: 'Backdoor', 'Easy shape' or 'Airdrop slab' ")
-        barrel_type = barrel_type.lower()
+        barrel_type = input("Which kind of barrel? choose: 'Backdoor', 'Easy shape' or 'Airdrop slab' ").lower()
+
         if barrel_type == "backdoor":
             print(f"{name}, you was dropped-in by locals. You so hungry for waves, they said. You dead. Game over ;(")
         elif barrel_type == "easy shape":
