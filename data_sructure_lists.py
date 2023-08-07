9b1ede2813fbd7bbb242aff3d723a4cba396add8
RoyalDumbSurfer <comradodean@gmail.com>
2023-08-07 19:04:11 +0800

    lists

---
+++ data_sructure_lists.py
@@ -0,0 +1,14 @@
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland",
+                     "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island",
+                     "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois",
+                     "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin",
+                     "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado",
+                     "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma",
+                     "New Mexico", "Arizona", "Alaska", "Hawaii"]
+new_list = []
+for i in states_of_america:
+    new_list.append(i)
+
+print(len(states_of_america))
+print(len(new_list))
+print(new_list.index)
