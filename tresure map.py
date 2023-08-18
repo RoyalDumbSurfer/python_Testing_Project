36d32c06e927b1537b1e19be9508110205ca1cbc
RoyalDumbSurfer <comradodean@gmail.com>
2023-08-18 16:37:26 +0800

    tresure island done

--- tresure map.py
+++ tresure map.py
@@ -1,14 +1,14 @@
-row1 = ["⬜️","️⬜️","️⬜️"]
-row2 = ["⬜️","⬜️","️⬜️"]
-row3 = ["⬜️️","⬜️️","⬜️️"]
+row1 = ["😁️", "🤣️", "😉️"]
+row2 = ["🤩️", "😭️", "😴️"]
+row3 = ["😹", "🤏", "🤑"]
 map = [row1, row2, row3]
 print(f"{row1}\n{row2}\n{row3}")
 position = input("Where do you want to put the treasure? ")
 
-
-
-
-
+column = int(position[1])
+row = int(position[0])
+treasure = 'x'
+map[column-1][row-1] = treasure
 
 
 print(f"{row1}\n{row2}\n{row3}")
