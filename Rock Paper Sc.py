b3f4aacbb6494ea7825d0667e049fb72c4169ccb
RoyalDumbSurfer <comradodean@gmail.com>
2023-08-24 20:31:25 +0800

    tic tac toe update4

--- Rock Paper Sc.py
+++ Rock Paper Sc.py
@@ -1,6 +1,15 @@
 import random
 
-rock_2 = '''
+scissors_0 = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+rock_1 = '''
     _______
 ---'   ____)
       (_____)
@@ -9,7 +18,7 @@ rock_2 = '''
 ---.__(___)
 '''
 
-paper_3 = '''
+paper_2 = '''
     _______
 ---'   ____)____
           ______)
@@ -18,41 +27,36 @@ paper_3 = '''
 ---.__________)
 '''
 
-scissors_1 = '''
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)
-'''
-game_images = [scissors_1, rock_2, paper_3]
+game_images = [scissors_0, rock_1, paper_2]
 
 print("Let's play!")
 user_name = input("What's your name?\n")
 print(f"Welcome to the Game, {user_name}!")
 
-user_choice = int(input("Choose one of the options: type '1'- scissors, type '2' - rock, type '3' - paper\n"))
-print(f"{user_name} choice:\n")
-print(game_images[user_choice])
+user_choice = int(input("Choose one of the options: type '0'- scissors, type '1' - rock, type '2' - paper\n"))
+ai_choice = random.randint(0, 2)
 
-ai_choice = random.randint(1, 3)
-print("AI choice:\n")
-print(game_images[ai_choice])
+if user_choice > 2:
+    print(f"You typed an invalid number, {user_name}!")
+else:
+    print(f"{user_name} choice:\n")
+    print(game_images[user_choice], "\n")
+    print("AI choice:\n")
+    print(game_images[ai_choice])
+
 
 if ai_choice == user_choice:
     print(f"Draw. Let's play again, {user_name}!  🤷🏻‍")
+elif ai_choice == 0 and user_choice == 1:
+    print(f"You Win, {user_name}! Congratulations!!! 🤴🏻")
+elif ai_choice == 0 and user_choice == 2:
+    print(f"You Lose, {user_name}! 🥱")
+elif ai_choice == 1 and user_choice == 0:
+    print(f"You Lose, {user_name}! 🥱")
 elif ai_choice == 1 and user_choice == 2:
     print(f"You Win, {user_name}! Congratulations!!! 🤴🏻")
-elif ai_choice == 1 and user_choice == 3:
-    print(f"You Lose, {user_name}! 🥱")
+elif ai_choice == 2 and user_choice == 0:
+    print(f"You Win, {user_name}! Congratulations!!! 🤴🏻")
 elif ai_choice == 2 and user_choice == 1:
     print(f"You Lose, {user_name}! 🥱")
-elif ai_choice == 2 and user_choice == 3:
-    print(f"You Lose, {user_name}! 🥱")
-elif ai_choice == 3 and user_choice == 1:
-    print(f"You Win, {user_name}! Congratulations!!! 🤴🏻")
-elif ai_choice == 3 and user_choice == 2:
-    print(f"You Lose, {user_name}! 🥱")
-else:
-    print(f"You typed an invalid number, {user_name}!")
+
