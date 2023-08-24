034984e3951d1c33c251bb4ebe95b82e8bbc2126
RoyalDumbSurfer <comradodean@gmail.com>
2023-08-24 19:53:19 +0800

    tic tac toe update

--- Rock Paper Sc.py
+++ Rock Paper Sc.py
@@ -34,7 +34,7 @@ user_name = input("What's your name?\n")
 print(f"Welcome to the Game, {user_name}!")
 user_choice = int(input("Choose one of the options: type '1'- scissors, type '2' - rock, type '3' - paper\n"))
 
-if ai_choice == 1 and user_choice == 1:
+if ai_choice == user_choice:
     print(f"{scissors_1}\n{scissors_1}")
     print(f"Draw. Let's play again, {user_name}!  🤷🏻‍")
 elif ai_choice == 1 and user_choice == 2:
@@ -43,23 +43,15 @@ elif ai_choice == 1 and user_choice == 2:
 elif ai_choice == 1 and user_choice == 3:
     print(f"{scissors_1}\n{paper_3}")
     print(f"You Lose, {user_name}! 🥱")
-
 elif ai_choice == 2 and user_choice == 1:
     print(f"{rock_2}\n{scissors_1}")
     print(f"You Lose, {user_name}! 🥱")
-elif ai_choice == 2 and user_choice == 2:
-    print(f"{rock_2}\n{rock_2}")
-    print(f"Draw. Let's play again, {user_name}!  🤷🏻‍")
 elif ai_choice == 2 and user_choice == 3:
     print(f"{rock_2}\n{paper_3}")
     print(f"You Lose, {user_name}! 🥱")
-
 elif ai_choice == 3 and user_choice == 1:
     print(f"{paper_3}\n{scissors_1}")
     print(f"You Win, {user_name}! Congratulations!!! 🤴🏻")
 elif ai_choice == 3 and user_choice == 2:
     print(f"{paper_3}\n{rock_2}")
     print(f"You Lose, {user_name}! 🥱")
-elif ai_choice == 3 and user_choice == 3:
-    print(f"{paper_3}\n{paper_3}")
-    print(f"Draw. Let's play again, {user_name}!  🤷🏻‍")
