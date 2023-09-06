6f90df7547029b5acb9bda47acb70b5921c739f1
RoyalDumbSurfer <comradodean@gmail.com>
2023-09-06 23:24:12 +0800

    sum of even2

--- sum_of_even.py
+++ sum_of_even.py
@@ -1,7 +1,16 @@
 sum_of_even = 0
 for i in range(0, 101, 2):
-    print(i)
     sum_of_even += i
 
 print("-------------\n")
 print(sum_of_even)
+
+
+
+sum_of_even_2 = 0
+for number in range(0, 101):
+    if number % 2 == 0:
+        sum_of_even_2 += number
+
+print("-------------\n")
+print(sum_of_even_2)
