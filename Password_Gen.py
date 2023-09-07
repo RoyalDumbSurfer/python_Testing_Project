ddf8bc1cab698837595ec7fb0f41b8e7db9946ce
RoyalDumbSurfer <comradodean@gmail.com>
2023-09-07 23:03:07 +0800

    pass gen v1.0

---
+++ Password_Gen.py
@@ -0,0 +1,29 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+new_letters = []
+for l in range(0, nr_letters):
+    new_letters += random.choice(letters)
+
+new_numbers = []
+for n in range(0, nr_numbers):
+    new_numbers += random.choice(numbers)
+
+new_symbols = []
+for s in range(0, nr_symbols):
+    new_symbols += random.choice(symbols)
+
+
+new_pass = (new_letters + new_symbols + new_numbers)
+
+random.shuffle(new_pass)
+for t in new_pass:
+    print(t, end='')
+
