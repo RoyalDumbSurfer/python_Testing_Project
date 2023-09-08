9b9234ed7075b6860a15250bd9f9e02d9577bee9
RoyalDumbSurfer <comradodean@gmail.com>
2023-09-08 11:43:35 +0800

    pass gen v1.1

--- Password_Gen.py
+++ Password_Gen.py
@@ -5,8 +5,8 @@ symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
 nr_letters = int(input("How many letters would you like in your password?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+nr_symbols = int(input("How many symbols would you like?\n"))
+nr_numbers = int(input("How many numbers would you like?\n"))
 
 new_letters = []
 for l in range(0, nr_letters):
@@ -24,6 +24,8 @@ for s in range(0, nr_symbols):
 new_pass = (new_letters + new_symbols + new_numbers)
 
 random.shuffle(new_pass)
+
+print("Your super strong password: ")
 for t in new_pass:
     print(t, end='')
 
